460077993d5c71de652493c3f252190f24192554
vhietala <vhietala@gmail.com>
2018-04-24 22:30:50 +0300

    Week6 Tuesday commit still working with Events

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const logger = require('morgan');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const eventsRouter = require('./routes/events');
+const bodyParser = require('body-parser');
 
 const app = express();
 
@@ -15,12 +16,14 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(bodyParser);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/events', eventsRouter);
--- routes/events.js
+++ routes/events.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const router = express.Router();
-const event = require('../models/teamEvent');
+const TeamEvent = require('../models/teamEvent');
 
 /**
  * @api {get} /events Request all events from database
@@ -13,13 +13,30 @@ const event = require('../models/teamEvent');
  *
  */
 app.get('/', (req, res) => {
-  event.find().then(data => {
+  TeamEvent.find().then(data => {
     console.log(data);
     res.send(data);
   }, err => {
     res.send(err.error.message);
   });
 });
+
+
+/**
+ *  @api {post} /events creates a new TeamEvent
+ *  @apiName PostEvent
+ *  @apiGroup Event
+ *  @apiDescription This is used to create new TeamEvents
+ *  uses JSON as input
+ *
+ */
+
+router.post('/',(req,res)=>{
+    TeamEvent.create(req.body).then(resp=>{
+        res(resp);
+    });
+});
+
 
 /**
  *  @api {get} /events/:param Return events based on search paramater
@@ -32,10 +49,51 @@ app.get('/', (req, res) => {
 router.get('/:param', (req, res) => {
   const parameter = req.params.param;
   console.log(parameter);
-  event.findOne({'title': parameter}).then(data => {
+  TeamEvent.findOne({'title': parameter}).then(data => {
     console.log(data);
     res.send(data);
   }, err => {
     res.send(err.error.message);
   });
 });
+
+
+/**
+ *  @api {put} /events/:param Return events based on search paramater
+ *  @apiName PutEvent
+ *  @apiGroup Event
+ *  @apiDescription This one can be used to search for events based on the title
+ *  it returns one item as a JSON.
+ *
+ */
+router.put('/:param', (req, res) => {
+    const parameter = req.params.param;
+    console.log(parameter);
+    TeamEvent.findOne({'title': parameter}).then(data => {
+        console.log(data);
+        res.send(data);
+    }, err => {
+        res.send(err.error.message);
+    });
+});
+
+
+/**
+ *  @api {delete} /events/:param Return events based on search paramater
+ *  @apiName DeleteEvent
+ *  @apiGroup Event
+ *  @apiDescription This is used to remove one TeamEvent from the database
+ */
+router.get('/:param', (req, res) => {
+    const parameter = req.params.param;
+    console.log(parameter);
+    TeamEvent.findOneAndRemove({'title': parameter}).then(data => {
+        remove(data);
+        console.log(data);
+        res.send(data);
+    }, err => {
+        res.send(err.error.messsage);
+    });
+});
+
+module.exports = router;

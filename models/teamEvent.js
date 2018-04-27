d1cba2973a11157fc001641540e012e947bb9d21
vhietala <vhietala@gmail.com>
2018-04-27 11:55:14 +0300

    event create

--- models/teamEvent.js
+++ models/teamEvent.js
@@ -3,21 +3,26 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const eventSchema = new Schema({
-    time: {
-        type: Date,
-        required: true
-    },
-    location: {
-        type: String,
-        required: true
-    },
-    category: {
-        type: String
-    },
-    title: {
-        type: String,
-        required: true
-    },
+  time: {
+    type: Date,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  info: String,
+  comments:{
+    user: String,
+    comment: String
+  }
 });
 
 const TeamEvent = mongoose.model('teamEvent', eventSchema);

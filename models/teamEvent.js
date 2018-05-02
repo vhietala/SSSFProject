8714205ed91288bf1da65fa43da76df4071af608
Ville Hietala <vhietala@gmail.com>
2018-05-02 18:53:30 +0300

    close: event database

--- models/teamEvent.js
+++ models/teamEvent.js
@@ -2,27 +2,29 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const commentSchema = new Schema({
+        user: String,
+        comment: String
+    }
+);
 const eventSchema = new Schema({
-  time: {
-    type: Date,
-    required: true,
-  },
-  location: {
-    type: String,
-    required: true,
-  },
-  category: {
-    type: String,
-  },
-  title: {
-    type: String,
-    required: true,
-  },
-  info: String,
-  comments:{
-    user: String,
-    comment: String
-  }
+    time: {
+        type: Date,
+        required: true,
+    },
+    location: {
+        type: String,
+        required: true,
+    },
+    category: {
+        type: String,
+    },
+    title: {
+        type: String,
+        required: true,
+    },
+    info: String,
+    comments: commentSchema,
 });
 
 const TeamEvent = mongoose.model('teamEvent', eventSchema);

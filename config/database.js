bb4601ec6701c7604aa5028f1d7b875070e36fd5
Ville Hietala <vhietala@gmail.com>
2018-05-07 12:11:10 +0300

    Jelastic deployment stuff

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 'use strict';
 
 module.exports = {
-    'url' : `mongodb://${process.env.DB_HOST}/${process.env.DB_NAME}`
+    'url' : `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_NAME}`
 };

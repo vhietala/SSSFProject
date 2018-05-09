2198929a5a3a534c6a3d2957c66d1eaa5d54a880
Ville Hietala <vhietala@gmail.com>
2018-05-09 15:57:50 +0300

    Heroku deployment stuff

--- config/database.js
+++ config/database.js
@@ -2,5 +2,5 @@
 const dontenv =  require('dotenv')
 
 module.exports = {
-    'url' : `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
+    'url' : `${process.env.MONGODB_URI}`
 };
